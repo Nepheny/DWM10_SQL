e570f8e0d1e8143f21069648e1669952e75fb953
admin <admin@iMac3.local>
2018-05-15 17:20:45 +0200

    Using safe request

--- provision/install.sh
+++ provision/install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#install programms
 sudo apt update
-sudo apt install -y mysql-server
+sudo apt install -y mysql-server phpmyadmin
 sudo apt install -y vim curl git wget
+
+#create a user for mysql and add the database
+sudo su
+mysql --database=mysql -e "CREATE USER 'crystal'@'localhost' IDENTIFIED BY 'crystal';"
+mysql --database=mysql -e "CREATE USER 'crystal'@'%' IDENTIFIED BY 'crystal';"
+mysql --database=mysql -e "GRANT ALL ON *.* TO 'crystal'@'localhost';"
+mysql --database=mysql -e "GRANT ALL ON *.* TO 'crystal'@'%';"
+mysql -e /vagrant/provision/blog.sql
+exit
+
+#configure VM
+sudo cp /provision/php.ini /etc/php/7.0/apache2/php.ini
+sudo rm -r /var/www/html/
+sudo ln -s /vagrant/web /var/www/html
